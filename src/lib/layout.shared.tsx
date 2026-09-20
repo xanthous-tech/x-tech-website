8f3670082d98aa583a037e348231a676cb778b89
Langoustine <langoustine@divby0.io>
2026-09-20 22:13:21 +0800

    Use company logo in site title and favicon

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,9 +1,24 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
+import logo from '@/app/icon.png';
 import { appName } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
-    nav: { title: appName, url: '/' },
+    nav: {
+      title: (
+        <>
+          <Image
+            src={logo}
+            alt=""
+            className="h-8 w-auto shrink-0"
+            unoptimized
+          />
+          {appName}
+        </>
+      ),
+      url: '/',
+    },
     githubUrl: 'https://github.com/xanthous-tech',
   };
 }
